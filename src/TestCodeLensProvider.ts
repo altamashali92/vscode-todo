bfb5beaacfaf5653d1c1aff2fdd911c2a53b871c
Altamash Ali <mali2@atlassian.com>
2024-11-28 20:10:25 +0530

    implements Codelensprovider

--- src/TestCodeLensProvider.ts
+++ src/TestCodeLensProvider.ts
@@ -4,20 +4,20 @@ import {
   Range,
   TextDocument,
   window,
+  CodeLensProvider,
+  ProviderResult,
 } from "vscode";
 import { parse } from "./parser";
 
-export class TestCodeLensProvider {
-  provideCodeLenses(
-    document: TextDocument,
-    token: CancellationToken
-  ): CodeLens[] {
+export class TestCodeLensProvider implements CodeLensProvider {
+  provideCodeLenses(document: TextDocument, token: CancellationToken): ProviderResult<CodeLens[]> {
     const codeLenses: CodeLens[] = [];
     const activeEditor = window.activeTextEditor;
-    if (activeEditor) {
-        const parsedTestFile = parse(activeEditor.document.fileName);
-        console.log("parsedTestFile", parsedTestFile);
+    if (!activeEditor) {
+      return [];
     }
+    const parsedTestFile = parse(activeEditor.document.fileName);
+    console.log("parsedTestFile", parsedTestFile);
     const regex = /it\(['"`](.*?)['"`],/g;
     const text = document.getText();
     let match;
@@ -26,9 +26,9 @@ export class TestCodeLensProvider {
       const line = document.positionAt(match.index).line;
       const range = new Range(line, 0, line, 0);
       const command = {
-        title: "Mark as Fixed",
-        command: "FlakeManager.markAsFixed",
-        arguments: [match[1]],
+        title: "Flaky",
+        command: "",
+        arguments: [],
       };
       codeLenses.push(new CodeLens(range, command));
     }
